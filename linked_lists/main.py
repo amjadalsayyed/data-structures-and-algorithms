557dfabeb94d353d8def60cbb80c0c5248aef5cd
amjad alsayyed <amgad.alsayyed@yahoo.com>
2023-05-04 18:24:38 +0300

    cc10

--- linked_lists/main.py
+++ linked_lists/main.py
@@ -1,5 +1,7 @@
 from linked_list import LinkedList
 
+is_palindrome = LinkedList.is_palindrome
+
 if __name__ == '__main__':
       linked_list = LinkedList()
       linked_list_2 = LinkedList()
@@ -10,20 +12,22 @@ if __name__ == '__main__':
       # linked_list.append('D')
 
 
+      linked_list_2.insert('1')
+      linked_list_2.insert('2')
       linked_list_2.insert('3')
       linked_list_2.insert('2')
       linked_list_2.insert('1')
       # linked_list_2.insert('4')
-     
+      print(linked_list_2)
+      # linked_list_2.revers()
      
       
-      print(linked_list)
-      print(linked_list_2)
+      # print(is_palindrome(linked_list_2))
       # print(linked_list.count)
 
       # print(linked_list.kthFromEnd(4))
 
-      linked_list_3 = LinkedList()
-      # linked_list_3.zip_list(linked_list,linked_list_2)
-      print(linked_list_3.zip_list(linked_list,linked_list_2))
+      # linked_list_3 = LinkedList()
+      # # linked_list_3.zip_list(linked_list,linked_list_2)
+      # print(linked_list_3.zip_list(linked_list,linked_list_2))
 
