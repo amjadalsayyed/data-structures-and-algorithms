b4e96ec734355d32ec2aad2bb605869809f63624
amjad alsayyed <amgad.alsayyed@yahoo.com>
2023-07-17 14:27:57 +0300

    done

--- graph/graph.md
+++ graph/graph.md
@@ -1,1 +1,73 @@
-# graph
+# Code Challenge 35
+
+## Class of hash table
+
+## add_vertex function
+
+### arguments : value
+
+### returns: new vertex
+
+### big O
+
+#### time : O(1) space : O(1)
+
+### Approach
+
+#### this function gets a value and creates a new node from it then adds it to the adj_list as a key with an empty list as a value for that key
+
+## add_edge function
+
+### arguments : vertex1, vertex2, weight : optional
+
+### returns: nothing
+
+### big O
+
+#### time : O(1) space : O(1)
+
+### Approach
+
+#### this function gets called with to vertices and an optional weight parameter then checks if the two vertices exist in the adj_list then create an edg from both vertices and add them to the list of edges for each one of them
+
+## get_vertices function
+
+### arguments : None
+
+### returns: a list of vertices
+
+### big O
+
+#### time : O(n) space : O(n)
+
+### Approach
+
+#### this function is called with no arguments and returns a list of vertices
+
+## get_neighbors function
+
+### arguments : vertex
+
+### returns: list of edges of the vertex
+
+### big O
+
+#### time : O(n) space : O(n)
+
+### Approach
+
+#### this function is called with a vertex as an argument and returns a list of edges connected to it
+
+## get_size function
+
+### arguments : none
+
+### returns: size
+
+### big O
+
+#### time : O(1) space : O(1)
+
+### Approach
+
+#### this function is called with no arguments and returns the size of the graph
---
+++ tests/test_graph.py
@@ -0,0 +1,71 @@
+from graph.graph import Graph
+def test_graph_one():
+    graph = Graph()
+
+    graph.add_vertex("A")
+    actual = str(graph)
+    expected = "A -> \n"
+    assert actual == expected
+
+def test_graph_two():
+    graph = Graph()
+
+    a = graph.add_vertex("A")
+    b = graph.add_vertex("B")
+    graph.add_edge(a,b,2)
+    actual = str(graph)
+    expected = "A -> B -----> \nB -> A -----> \n"
+    assert actual == expected
+
+def test_graph_three():
+    graph = Graph()
+
+    a = graph.add_vertex("A")
+    b = graph.add_vertex("B")
+    c = graph.add_vertex("C")
+    d = graph.add_vertex("D")
+    actual = graph.get_vertices()
+    expected =['A', 'B', 'C', 'D']
+    assert actual == expected
+
+def test_graph_four():
+    graph = Graph()
+
+    a = graph.add_vertex("A")
+    b = graph.add_vertex("B")
+    c = graph.add_vertex("C")
+    d = graph.add_vertex("D")
+    graph.add_edge(a,b,2)
+    graph.add_edge(a,c,3)
+    graph.add_edge(c,b,3)
+    graph.add_edge(d,b,4)
+    graph.add_edge(d,c,5)
+    actual = graph.get_neighbors(d)
+    expected =[('B', 4), ('C', 5)]
+    assert actual == expected
+
+def test_graph_five():
+    graph = Graph()
+
+    a = graph.add_vertex("A")
+    b = graph.add_vertex("B")
+    c = graph.add_vertex("C")
+    d = graph.add_vertex("D")
+    graph.add_edge(a,b,2)
+    graph.add_edge(a,c,3)
+    graph.add_edge(c,b,3)
+    graph.add_edge(d,b,4)
+    graph.add_edge(d,c,5)
+    actual = graph.size()
+    expected =4
+    assert actual == expected
+
+def test_graph_five():
+    graph = Graph()
+
+    a = graph.add_vertex("A")
+
+    graph.add_edge(a,None,2)
+    actual = str(graph)
+    expected ="A -> \n"
+    assert actual == expected
